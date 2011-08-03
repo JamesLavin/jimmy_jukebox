59649a17a19c1c1a0aca54f171e208d34f91609a
James Lavin <james@jameslavin.com>
2011-08-03 10:14:43 -0400

    make mp3 & ogg search in jimmy_jukebox.rb case-insensitive; rename variables

--- lib/jimmy_jukebox.rb
+++ lib/jimmy_jukebox.rb
@@ -106,14 +106,14 @@ module JimmyJukebox
     end
 
     def generate_directories_list
-      @mp3_directories = []
+      @music_directories = []
       # ARGV[0] can be "jazz.txt" (a file holding directory names), "~/Music/JAZZ" (a directory path) or nil
       if is_a_txt_file?(ARGV[0])
         set_music_directories_from_file
       elsif is_a_directory?(ARGV[0])
-        @mp3_directories << File.expand_path(ARGV[0])
+        @music_directories << File.expand_path(ARGV[0])
       else
-        @mp3_directories << File.expand_path(DEFAULT_MP3_DIRECTORY)
+        @music_directories << File.expand_path(DEFAULT_MP3_DIRECTORY)
       end
       add_all_subdirectories
     end
@@ -132,26 +132,26 @@ module JimmyJukebox
       File.open(@music_directories_file, "r") do |inf|
         while (line = inf.gets)
           line.strip!
-          @mp3_directories << File.expand_path(line)
+          @music_directories << File.expand_path(line)
         end
       end
     end
 
     def add_all_subdirectories
       new_dirs = []
-      @mp3_directories.each do |dir|
+      @music_directories.each do |dir|
         Dir.chdir(dir)
         new_dirs += Dir.glob("**/")
       end
-      @mp3_directories += new_dirs
+      @music_directories += new_dirs
     end
 
     def generate_song_list
       @songs = []
-      @mp3_directories.each do |mp3_dir|
-        files = Dir.entries(File.expand_path(mp3_dir))
-        files.delete_if { |f| !f.match(/.*\.mp3/) && !f.match(/.*\.ogg/) }
-        files.map! { |f| File.expand_path(mp3_dir) + '/' + f }
+      @music_directories.each do |music_dir|
+        files = Dir.entries(File.expand_path(music_dir))
+        files.delete_if { |f| !f.match(/.*\.mp3/i) && !f.match(/.*\.ogg/i) }
+        files.map! { |f| File.expand_path(music_dir) + '/' + f }
         @songs = @songs + files
       end
       #songs = ["~/Music/Artie_Shaw/Georgia On My Mind 1941.mp3",
@@ -159,11 +159,11 @@ module JimmyJukebox
     end
 
     def play_file(music_file)
-      if music_file.match(/\.mp3$/) && @mpg123_installed
+      if music_file =~ /\.mp3$/i && @mpg123_installed
         system_yield_pid("mpg123", File.expand_path(music_file)) do |pid|
           @playing_pid = pid 
         end
-      elsif music_file.match(/\.ogg$/) && @ogg123_installed
+      elsif music_file =~ /\.ogg$/i && @ogg123_installed
         system_yield_pid("ogg123", File.expand_path(music_file)) do |pid|
           @playing_pid = pid 
         end
