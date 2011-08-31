008faa85d3e6ba303afa16d58b1275bf574816f8
James Lavin <james@jameslavin.com>
2011-08-31 12:05:18 -0400

    change Jukebox from class into module

--- lib/jimmy_jukebox.rb
+++ lib/jimmy_jukebox.rb
@@ -1,47 +1,48 @@
-require 'shellwords'
-
 module JimmyJukebox
 
   # make system call and get pid so you can terminate process
-  def system_yield_pid(cmd)
+  def system_yield_pid(player, filename)
     # would like to use Process.respond_to?(:fork) but JRuby mistakenly returns true
     if (defined?(JRUBY_VERSION) || RUBY_PLATFORM == 'java')
-      pid = Spoon.spawnp(cmd)
+      pid = Spoon.spawnp("#{player} #{filename}")
     else
       begin
         pid = fork do             # creates and runs block in subprocess (which will terminate with status 0), capture subprocess pid
-          exec(cmd)               # replaces current process with system call
+          exec(player,filename)   # replaces current process with system call
           exit! 127               # exit process and return exit status 127; should never be reached
         end
       rescue NotImplementedError
         raise "*** fork()...exec() not supported ***"
       end
     end
+    if pid
+      puts "pid: #{pid}"
+    else
+      puts "No process id (pid)!"
+      raise "@current_song: #{@current_song.inspect}"
+    end
     yield pid if block_given? # call block, passing in the subprocess pid
     Process.waitpid(pid)      # Waits for a child process to exit, returns its process id, and sets $? to a Process::Status object
     $?                        # return Process::Status object with instance methods .stopped?, .exited?, .exitstatus; see: http://www.ruby-doc.org/core/classes/Process/Status.html
   end
 
-  class Jukebox
+  module Jukebox
 
     require 'jimmy_jukebox/user_config'
+    require 'jimmy_jukebox/song'
 
-    attr_reader :continuous_play, :current_song_paused, :playing_pid, :music_player
+    attr_reader :continuous_play, :current_song
 
-    def initialize
-      @user_config = UserConfig.new
-    end
-
-    def play_loop
+    def self.play_loop
       @continuous_play = true
       while @continuous_play do
-        play
+        play_once
       end
     end
 
-    def play
+    def self.play_once
       begin
-        play_random_song(@user_config.songs)
+        play_random_song
       rescue SystemExit, Interrupt => e
         terminate_current_song
         puts "\nMusic terminated by user"
@@ -49,79 +50,59 @@ module JimmyJukebox
       end
     end
 
-    def quit
+    def self.quit
       stop_looping
       terminate_current_song
     end
 
-    def skip_song
+    def self.skip_song
+      if @current_song
+        puts "Terminating #{@current_song.music_file}"
+      else
+        raise "No @current_song"
+      end
       terminate_current_song
     end
 
-    def pause_current_song
-      @current_song_paused = true
-      # jruby doesn't seem to handle system() correctly
-      # trying backticks
-      # system("kill -s STOP #{@playing_pid}") if @playing_pid
-      `kill -s STOP #{@playing_pid}` if @playing_pid
+    def self.pause_current_song
+      @current_song.pause
     end
 
-    def unpause_current_song
-      @current_song_paused = false
-      # jruby doesn't seem to handle system() correctly
-      # trying backticks
-      #system("kill -s CONT #{@playing_pid}") if @playing_pid
-      `kill -s CONT #{@playing_pid}` if @playing_pid
+    def self.unpause_current_song
+      @current_song.unpause
     end
 
-    private
-
-    def stop_looping
+    def self.stop_looping
       @continuous_play = false
     end
 
-    def play_random_song(songs)
-      terminate_current_song
+    def self.songs
+      user_config.songs
+    end
+
+    def self.play_random_song
+      #terminate_current_song
       raise "JimmyJukebox has no songs to play!" if songs.length == 0
-      play_file( songs[rand(songs.length)] )
+      @current_song = Song.new( songs[rand(songs.length)] )
+      puts @current_song.inspect
+      @current_song.play(user_config)
+      @current_song.terminate # ????
+      @current_song = nil # ????
     end
 
-    def terminate_current_song
-      if @playing_pid
-        @current_song_paused = false
-        `killall #{@music_player}`
-        @music_player = nil
-        # killing processes seems problematic in JRuby
-        # I've tried several approaches, and nothing seems reliable
-        #Process.kill("SIGKILL",@playing_pid)
-        #Process.kill("SIGTERM",@playing_pid)
-        #`kill #{@playing_pid}` if @playing_pid
-        @playing_pid = nil
-      end
-    end
-
-    def play_file(music_file)
-      # TODO: refactor the duplicate code below into a method
-      if music_file =~ /\.mp3$/i && @user_config.mp3_player
-        process_status = play_file_with(music_file, @user_config.mp3_player)
-      elsif music_file =~ /\.ogg$/i && @user_config.ogg_player
-        process_status = play_file_with(music_file, @user_config.ogg_player)
+    def self.terminate_current_song
+      if @current_song
+        puts "Terminating #{@current_song.music_file}"
+        @current_song.terminate
+        @current_song = nil
       else
-        raise "Attempted to play a file format this program cannot play"
+        puts "No song is currently playing"
       end
-      process_status.exitstatus.to_i == 0 ? (@playing_pid = nil) : (raise "Experienced a problem playing a song")
     end
 
-    def play_file_with(music_file,player)
-      puts "Press Ctrl-C to stop the music and exit this program"
-      puts "Now playing '#{music_file}'"
-      @music_player = player
-      #`#{player} #{File.expand_path(music_file).shellescape}`
-      system_yield_pid("#{player} #{File.expand_path(Shellwords.shellescape(music_file))}") do |pid|
-      puts "#{player} #{File.expand_path(Shellwords.shellescape(music_file))}"
-      #system_yield_pid(player, File.expand_path(music_file)) do |pid|
-        @playing_pid = pid 
-      end
+    def self.user_config
+      @user_config = UserConfig.new unless @user_config
+      @user_config
     end
 
   end
