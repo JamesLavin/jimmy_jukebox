fb103e010817ba8d20beefa73fc5ac492d3cb6f3
James Lavin <james@jameslavin.com>
2012-10-09 14:44:27 -0400

    cleaning-up-tests-driven-refactoring

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,14 +4,16 @@ RSpec.configure do |config|
   end
 end
 
+require 'fakefs/spec_helpers'
+
 # Override exec() to prevent songs from actually playing
 # Instead, start a brief sleep process
-module Kernel
-  alias :real_exec :exec
-
-  def exec(*cmd)
-    real_exec("sleep 0.2")  
-  end
-end
+#module Kernel
+#  alias :real_exec :exec
+#
+#  def exec(*cmd)
+#    real_exec("sleep 0.2")  
+#  end
+#end
 
 
