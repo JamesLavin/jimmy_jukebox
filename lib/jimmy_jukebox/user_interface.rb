e64a8de1f1937a1d471e5f455d6499acb866451e
James Lavin <james@jameslavin.com>
2013-03-27 17:22:26 -0400

    move REPL out of thread into new UserInputHandler object

--- lib/jimmy_jukebox/user_interface.rb
+++ lib/jimmy_jukebox/user_interface.rb
@@ -9,59 +9,78 @@ play_loop_thread = Thread.new do
   jj.play_loop
 end
 
+module JimmyJukebox
+
+  class UserInputHandler
+  
+    class NoPlayLoopThreadException < Exception; end
+    
+    attr_accessor :jukebox
+
+    def initialize(jukebox)
+      self.jukebox = jukebox
+    end
+
+    def set_get_char_method
+      @get_char_method = if JimmyJukebox::RUNNING_JRUBY
+                           lambda { STDIN.getch }
+                         else
+                           lambda {
+                             begin
+                               stty_state = `stty -g`
+                               system("stty raw opost -echo -icanon isig")
+                               STDIN.getc.chr
+                             ensure
+                               `stty #{stty_state}`
+                             end
+                           }
+                         end
+    end
+
+    def get_char
+      @get_char_method.call
+    end
+  
+    def repl
+      set_get_char_method
+      loop do
+        case char = get_char
+        when "q", "Q"
+          raise Interrupt
+        when "e", "E"
+          jukebox.erase_song
+        when "p", "P"
+          if jukebox.current_song.paused?
+            jukebox.unpause_current_song
+          else
+            puts "Pausing. To unpause, enter 'p' again"
+            jukebox.pause_current_song
+          end
+        when "r", "R"
+          jukebox.replay_previous_song
+        when "s", "S"
+          jukebox.skip_song
+        else
+          puts "#{char.strip} is not a valid response" if char
+        end
+      end
+    rescue Interrupt, SystemExit => e
+      puts "JimmyJukebox closed by user request. Bye!"
+      jukebox.quit
+      exit
+    end
+
+  end
+
+end
+
+uih = UserInputHandler.new(jj)
+
 user_input_thread = Thread.new do
   
-  class NoPlayLoopThreadException < Exception; end
+  raise NoPlayLoopThreadException, "Can't find play_loop_thread" unless play_loop_thread
+  uih.repl
 
-  def set_get_char_method
-    @get_char_method = if JimmyJukebox::RUNNING_JRUBY
-                         lambda { STDIN.getch }
-                       else
-                         lambda {
-                           begin
-                             stty_state = `stty -g`
-                             system("stty raw opost -echo -icanon isig")
-                             STDIN.getc.chr
-                           ensure
-                             `stty #{stty_state}`
-                           end
-                         }
-                       end
-  end
-
-  def get_char
-    @get_char_method.call
-  end
-
-  begin
-    set_get_char_method
-    loop do
-      case char = get_char
-      when "q", "Q"
-        raise Interrupt
-      when "e", "E"
-        jj.erase_song
-      when "p", "P"
-        raise NoPlayLoopThreadException, "Can't find play_loop_thread" unless play_loop_thread
-        if jj.current_song.paused?
-          jj.unpause_current_song
-        else
-          puts "Pausing. To unpause, enter 'p' again"
-          jj.pause_current_song
-        end
-      when "r", "R"
-        jj.replay_previous_song
-      when "s", "S"
-        jj.skip_song
-      else
-        puts "#{char.strip} is not a valid response" if char
-      end
-    end
-  rescue Interrupt, SystemExit => e
-    puts "JimmyJukebox closed by user request. Bye!"
-    jj.quit
-    exit
-  end
 end
 
 play_loop_thread.join
