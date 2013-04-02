950a563585c8a69fac5be731e70f77c778f198a6
James Lavin <james@jameslavin.com>
2013-04-02 09:12:19 -0400

    add simple script for extracting urls

---
+++ lib/utils/extract_urls.rb
@@ -0,0 +1,30 @@
+require 'nokogiri'
+require 'open-uri'
+
+url = 'http://archive.org/details/ChubbyChecker-09-60'
+
+doc = Nokogiri::HTML(open(url))
+
+def all_links(doc)
+  links = []
+  doc.css('a').each do |link|
+    links << link['href']
+  end
+  links
+end
+
+def only_mp3s(links)
+  links.delete_if { |l| !l.match(/\.mp3$/i) }
+end
+
+def add_archive_org(links)
+  links.map! { |l| 'http://archive.org' + l }
+end
+
+def add_leading_dash(links)
+  links.map! { |l| '- ' + l }
+end
+
+links = add_leading_dash(add_archive_org(only_mp3s(all_links(doc))))
+
+puts links
